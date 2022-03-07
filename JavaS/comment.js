0437f2bee0c76d8d277dd4d780c69912c6746919
Sid <siddhant.khare.sk@gmail.com>
2022-03-07 18:25:17 +0530

    Var, DataTypes and comments in Js

---
+++ JavaS/comment.js
@@ -0,0 +1,6 @@
+var number = 5; // in-;ine comment
+
+/* This is a multi
+line comment*/
+
+var num = 2;
---
+++ JavaS/dattype.js
@@ -0,0 +1,32 @@
+var number = 5; // in-;ine comment
+
+/* This is a multi
+line comment*/
+
+var num = 2;
+
+/*Data types
+
+undefined, null, boolean, string, symbol, number and object*/
+
+var myName = "Sid";
+console.log(myName);
+myName = 8;
+console.log(myName);
+
+
+let ourName = "Tyson";
+
+const pi = 3.14; // Const is an unchangeable variable
+
+// Difference between assigning and asserting
+
+var a; //Asserting that a exists or we can say we declared it
+
+var b = 10; // We have assigned a value of 10 to b
+
+a = 7;
+console.log(b) // prints out 10
+
+b = a; //Assigning the value of a = 7 to b
+console.log(b) // But now it prints out 7 
